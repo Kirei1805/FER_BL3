a1ab611a1a62a59f29c6a3631635e0eb67a5046c
Kirei1805 <loiphan1805@gmail.com>
2025-08-11 10:37:54 +0700

    nop bai

---
+++ Slot1/ex1/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {}
+      <h1>Hello react</h1>
+      <p>My name is Phan Tấn Lợi</p>
+    </div>
+  );
+}
+
+export default App;
