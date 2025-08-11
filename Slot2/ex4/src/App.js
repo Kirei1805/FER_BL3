1c98be08661a03ae2ec1f8bbe9f2320dc150431b
Kirei1805 <loiphan1805@gmail.com>
2025-08-11 10:57:03 +0700

    nop bai

---
+++ Slot2/ex4/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Course names</h1>
+      <ul style={{ listStyleType: 'disc', textAlign: 'left', maxWidth: '200px', margin: '0 auto' }}>
+        <li>React</li>
+        <li>ReactNative</li>
+        <li>NodeJs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
